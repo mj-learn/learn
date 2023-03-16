e87bf2826dd98066df592857f3b4b9cdf8202e80
MOnjofn <monjofn@gmail.com>
2023-03-16 21:20:07 +0200

    add(lecture): control flow - if & else

---
+++ rust/03-complete-rust-dev-guide-ztm/02-making-decisions-with-rust/src/bin/01-control-flow-if-else.rs
@@ -0,0 +1,11 @@
+fn main() {
+    let age = 15;
+
+    println!("Age is {}", age);
+    
+    if age >= 21 {
+        println!("ok to purchase");
+    } else {
+        println!("cannot purchase");
+    }
+}
