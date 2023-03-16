6480b0dd9249975e78abaded9bfcc4ecd6a610c2
MOnjofn <monjofn@gmail.com>
2023-03-16 22:51:02 +0200

    add(exercise): basic match exercise 2

---
+++ rust/03-complete-rust-dev-guide-ztm/02-making-decisions-with-rust/src/bin/06-exercise-basic-match.rs
@@ -0,0 +1,24 @@
+// Topic: Decision making with match
+//
+// Program requirements:
+// * Display "one", "two", "three", or "other" based on whether
+//   the value of a variable is 1, 2, 3, or some other number,
+//   respectively
+//
+// Notes:
+// * Use a variable set to any integer
+// * Use a match expression to determine which message to display
+// * Use an underscore (_) to match on any value
+
+fn main() {
+    // * Use a variable set to any integer
+    let int = 2;
+
+    // * Use a match expression to determine which message to display
+    match int {
+        1 => println!("one"),
+        2 => println!("two"),
+        3 => println!("three"),
+        _ => println!("other")
+    }
+}
