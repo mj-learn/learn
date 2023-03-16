80ed4fcd2bf8bb494cd33e6922b9baf5190fe245
MOnjofn <monjofn@gmail.com>
2023-03-16 22:43:50 +0200

    add(lecture): basic match

---
+++ rust/03-complete-rust-dev-guide-ztm/02-making-decisions-with-rust/src/bin/04-basic-match.rs
@@ -0,0 +1,10 @@
+fn main() {
+    let my_name = "Bob";
+
+    match my_name {
+        "MOnjofn" => println!("That is my name"),
+        "Bob" => println!("Not my name"),
+        "Alice" => println!("Hello Alice"),
+        _ => println!("Nice to meet you!")
+    }
+}
