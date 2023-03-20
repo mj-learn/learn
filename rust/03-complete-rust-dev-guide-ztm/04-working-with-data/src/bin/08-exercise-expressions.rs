395fa515705d19a1b6e4a0aff47a76fc14b1f4f9
MOnjofn <monjofn@gmail.com>
2023-03-20 19:09:40 +0200

    add(exercise): expressions

---
+++ rust/03-complete-rust-dev-guide-ztm/04-working-with-data/src/bin/08-exercise-expressions.rs
@@ -0,0 +1,41 @@
+// Topic: Working with expressions
+//
+// Requirements:
+// * Print "its big" if a variable is > 100
+// * Print "its small" if a variable is <= 100
+//
+// Notes:
+// * Use a boolean variable set to the result of
+//   an if..else expression to store whether the value
+//   is > 100 or <= 100
+// * Use a function to print the messages
+// * Use a match expression to determine which message
+//   to print
+
+// * Use a function to print the messages
+fn message(bool: bool) {
+    // * Use a match expression to determine which message to print
+    match bool {
+        true => println!("its big"),
+        false => println!("its small"),
+    }
+}
+
+fn main() {
+    // * Use a boolean variable set to the result of
+    //   an if..else expression to store whether the value
+    //   is > 100 or <= 100
+
+    let value = 50;
+    let is_lt_100 = if value <= 100 {
+        false
+    } else {
+        true
+    };
+
+    let value_2 = 150;
+    let is_gt_100 = value_2 > 100;
+
+    message(is_lt_100);
+    message(is_gt_100);
+}
