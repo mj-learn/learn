018420a44ba09ad428c6ad9596ad902fb5618819
MOnjofn <monjofn@gmail.com>
2023-03-31 10:10:18 +0300

    refactor: rename struct name

--- rust/03-complete-rust-dev-guide-ztm/09-hashmap/src/bin/01-working-with-hashmaps.rs
+++ rust/03-complete-rust-dev-guide-ztm/09-hashmap/src/bin/01-working-with-hashmaps.rs
@@ -1,15 +1,15 @@
 use std::collections::HashMap;
 
 #[derive(Debug)]
-struct Contents {
+struct Content {
     content: String,
 }
 
 fn main() {
     let mut lockers = HashMap::new();
-    lockers.insert(1, Contents { content: "stuff".to_owned() });
-    lockers.insert(2, Contents { content: "shirts".to_owned() });
-    lockers.insert(3, Contents { content: "gym shorts".to_owned() });
+    lockers.insert(1, Content { content: "stuff".to_owned() });
+    lockers.insert(2, Content { content: "shirts".to_owned() });
+    lockers.insert(3, Content { content: "gym shorts".to_owned() });
 
     for (locker, content) in lockers.iter() {
         println!("locker: {:?}, content: {:?}", locker, content.content);
