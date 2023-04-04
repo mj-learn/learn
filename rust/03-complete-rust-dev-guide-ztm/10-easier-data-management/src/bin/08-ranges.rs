5c20bf86d750ba21b0f4e657a28890d22bf14ac4
MOnjofn <monjofn@gmail.com>
2023-04-04 13:10:55 +0300

    add(lecture): ranges

---
+++ rust/03-complete-rust-dev-guide-ztm/10-easier-data-management/src/bin/08-ranges.rs
@@ -0,0 +1,17 @@
+fn main() {
+    for num in 1..=4 {
+        println!("{:?}", num);
+    }
+
+    println!();
+
+    for num in 1..4 {
+        println!("{:?}", num);
+    }
+
+    println!();
+
+    for char in 'a'..='f' {
+        println!("{:?}", char);
+    }
+}
